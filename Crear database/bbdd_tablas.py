4385393d7e4f1e3f777f23d048108d6464cf3497
nacho dorado <natxo98@gmail.com>
2023-01-21 14:03:24 +0100

    tablas creadas

--- Crear database/bbdd_tablas.py
+++ Crear database/bbdd_tablas.py
@@ -1,3 +1,4 @@
+#Clase para crear la Base de datos
 class BaseDatos():
 
 	def __init__(self, nombre_bbdd, bbdd, cursor):
@@ -5,11 +6,12 @@ class BaseDatos():
 		self.bbdd=bbdd
 		self.cursor=cursor
 
+	#Crear la BBDD
 	def crear_bbdd(self):
 		self.cursor.execute(f"DROP DATABASE IF EXISTS {self.nombre_bbdd}")
 		self.cursor.execute(f"CREATE DATABASE {self.nombre_bbdd}")
 
-	
+#Clase para crear cada Tabla	
 class Tabla():
 	
 	def __init__(self, dataframe, nombre, nombre_bbdd, bbdd, cursor):
@@ -19,17 +21,20 @@ class Tabla():
 		self.bbdd=bbdd
 		self.cursor=cursor
 
+	#Convierte los df en listas
 	def convertir_df_lista(self):
 		diccionario=self.dataframe.to_dict("records")
 		lista=[list(i.values()) for i in diccionario]
 		return lista 
 
+	#Crea la tabla
 	def crear_tabla(self, consulta):
 		self.cursor.execute(f"USE {self.nombre_bbdd}")
 		self.cursor.execute(f"DROP TABLE IF EXISTS {self.nombre}")
 		self.cursor.execute(consulta)
 		return True
 
+	#Inserta los registros en la tabla
 	def insertar_registros(self, consulta, lista_registro):
 		self.cursor.execute(f"USE {self.nombre_bbdd}")
 		for i in lista_registro:
--- Crear database/crear_database.py
+++ Crear database/crear_database.py
@@ -32,6 +32,7 @@ for i in lista_rutas:
     else:
         pass
 
+#Creamos la conexion con MySQL
 configuracion=Config()
 conexion=configuracion.crear_conexion()
 bbdd=conexion[0]
@@ -39,15 +40,14 @@ c=conexion[1]
 
 
 #--------------------------------------------BASE DE DATOS---------------------------------------
-#creacion_bbdd=BaseDatos("pruebas", bbdd, c)
+nombre_bbdd="futbol"
+#creacion_bbdd=BaseDatos(nombre_bbdd, bbdd, c)
 #creacion_bbdd.crear_bbdd()
-
-
 
 
 #--------------------------------------------TABLA LIGAS---------------------------------------
 #Creamos un objeto tabla para las ligas
-tabla_ligas=Tabla(dfligas,"ligas", "pruebas", bbdd, c)
+tabla_ligas=Tabla(dfligas,"ligas", nombre_bbdd, bbdd, c)
 
 #Obtenemos los registros en forma de lista
 lista_ligas=tabla_ligas.convertir_df_lista()
@@ -76,3 +76,147 @@ consulta_creacion_ligas="""CREATE TABLE ligas
 consulta_insercion_ligas="""INSERT INTO ligas 
 VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 #insertar_tabla_ligas=tabla_ligas.insertar_registros(consulta_insercion_ligas, lista_ligas)
+
+
+#--------------------------------------------TABLA COMPETICIONES---------------------------------------
+#Creamos un objeto tabla para las competiciones
+tabla_competiciones=Tabla(dfcompeticiones,"competiciones", nombre_bbdd, bbdd, c)
+
+#Obtenemos los registros en forma de lista
+lista_competiciones=tabla_competiciones.convertir_df_lista()
+
+#Creamos la tabla competiciones
+consulta_creacion_competiciones="""CREATE TABLE competiciones 
+(CodCompeticion INT, 
+Competicion VARCHAR(70), 
+Temporada VARCHAR(50), 
+NumeroEquipos INT, 
+Campeon VARCHAR(50), 
+Pichichi VARCHAR(50), 
+Goles INT, 
+PRIMARY KEY (CodCompeticion),
+FOREIGN KEY (Competicion) REFERENCES ligas (NombreLiga) ON DELETE CASCADE)
+"""
+
+#creacion_tabla_competiciones=tabla_competiciones.crear_tabla(consulta_creacion_competiciones)
+
+#Insertamos los registros en la tabla competiciones
+consulta_insercion_competiciones="""INSERT INTO competiciones 
+VALUES(%s, %s, %s, %s, %s, %s, %s)"""
+#insertar_tabla_competiciones=tabla_competiciones.insertar_registros(consulta_insercion_competiciones, lista_competiciones)
+
+
+#--------------------------------------------TABLA ESTADIOS---------------------------------------
+#Creamos un objeto tabla para los estadios
+tabla_estadios=Tabla(dfestadios,"estadios", nombre_bbdd, bbdd, c)
+
+#Obtenemos los registros en forma de lista
+lista_estadios=tabla_estadios.convertir_df_lista()
+
+#Ponemos la posicion donde iria el codigo como None
+for i in lista_estadios:
+    i[0]=None
+
+#Creamos la tabla ligas
+consulta_creacion_estadios="""CREATE TABLE estadios 
+                                (CodEstadio INT AUTO_INCREMENT, 
+                                Nombre VARCHAR(70), 
+                                Pais VARCHAR(50), 
+                                Ciudad VARCHAR(50), 
+                                Latitud FLOAT, 
+                                Longitud FLOAT, 
+                                Capacidad VARCHAR(50), 
+                                PRIMARY KEY (CodEstadio))
+                                """
+
+#creacion_tabla_estadios=tabla_estadios.crear_tabla(consulta_creacion_estadios)
+
+#Insertamos los registros en la tabla estadios
+consulta_insercion_estadios="""INSERT INTO estadios 
+VALUES(%s, %s, %s, %s, %s, %s, %s)"""
+#insertar_tabla_estadios=tabla_estadios.insertar_registros(consulta_insercion_estadios, lista_estadios)
+
+
+#--------------------------------------------TABLA EQUIPOS---------------------------------------
+#Creamos un objeto tabla para las equipos
+tabla_equipos=Tabla(dfequipos,"equipos", nombre_bbdd, bbdd, c)
+
+#Obtenemos los registros en forma de lista
+lista_equipos=tabla_equipos.convertir_df_lista()
+
+#Creamos la tabla equipos
+consulta_creacion_equipos="""CREATE TABLE equipos 
+(NombreEquipo VARCHAR(50), 
+Codigo VARCHAR(6), 
+Pais VARCHAR(50), 
+CodEstadio INT, 
+Fundacion DATE, 
+LigasNacionales INT, 
+Champions INT, 
+MaximoGoleador VARCHAR(50), 
+Goles INT, 
+MaximoApariciones VARCHAR(50), 
+Partidos INT, 
+PRIMARY KEY (NombreEquipo), 
+FOREIGN KEY (CodEstadio) REFERENCES estadios (CodEstadio) ON DELETE CASCADE)"""
+
+#creacion_tabla_equipos=tabla_equipos.crear_tabla(consulta_creacion_equipos)
+
+#Insertamos los registros en la tabla equipos
+consulta_insercion_equipos="""INSERT INTO equipos 
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+#insertar_tabla_equipos=tabla_equipos.insertar_registros(consulta_insercion_equipos, lista_equipos)
+
+
+#--------------------------------------------TABLA PARTIDOS---------------------------------------
+#Creamos un objeto tabla para los partidos
+tabla_partidos=Tabla(dfpartidos,"partidos", nombre_bbdd, bbdd, c)
+
+#Obtenemos los registros en forma de lista
+lista_partidos=tabla_partidos.convertir_df_lista()
+
+#Ponemos la posicion donde iria el codigo como None
+for i in lista_partidos:
+    i.insert(0, None)
+
+#Creamos la tabla partidos
+consulta_creacion_partidos="""CREATE TABLE partidos 
+(CodPartido INT AUTO_INCREMENT, 
+Fecha DATE, 
+Hora VARCHAR(50), 
+ATM VARCHAR(50),
+Resultado VARCHAR(50),
+Equipo VARCHAR(50),
+Arbitro VARCHAR(50),
+CodEstadio INT,
+CodCompeticion INT,
+PRIMARY KEY (CodPartido),
+FOREIGN KEY (Equipo) REFERENCES equipos (NombreEquipo) ON DELETE CASCADE,
+FOREIGN KEY (CodEstadio) REFERENCES estadios (CodEstadio) ON DELETE CASCADE,
+FOREIGN KEY (CodCompeticion) REFERENCES competiciones (CodCompeticion))"""
+
+#creacion_tabla_partidos=tabla_partidos.crear_tabla(consulta_creacion_partidos)
+
+#Insertamos los registros en la tabla partidos
+consulta_insercion_partidos="""INSERT INTO partidos 
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+#insertar_tabla_partidos=tabla_partidos.insertar_registros(consulta_insercion_partidos, lista_partidos)
+
+
+
+#--------------------------------------------TABLA DESPLAZAMIENTOS---------------------------------------
+#Creamos un objeto tabla para los desplazamientos
+tabla_desplazamientos=Tabla(None,"desplazamientos", nombre_bbdd, bbdd, c)
+
+#Creamos la tabla desplazamientos
+consulta_creacion_desplazamientos="""CREATE TABLE desplazamientos 
+(CodDesplazamiento INT AUTO_INCREMENT,
+CodPartido INT,
+FechaIda DATE,
+FechaVuelta DATE,
+Acompañamiento VARCHAR(50), 
+Transporte VARCHAR(50),
+PRIMARY KEY (CodDesplazamiento),
+FOREIGN KEY (CodPartido) REFERENCES partidos (CodPartido) ON DELETE CASCADE)"""
+
+#creacion_tabla_desplazamientos=tabla_desplazamientos.crear_tabla(consulta_creacion_desplazamientos)
